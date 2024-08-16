8a3a007bb093da547869cec6bb394c4fcd022144
Alex Kennedy <akennedy4155@googlemail.com>
2024-08-16 15:08:11 -0600

    add a node to the node group

--- tf/blocky.tf
+++ tf/blocky.tf
@@ -36,8 +36,8 @@ resource "aws_eks_node_group" "blockchain-explorer-basic" {
     ]
 
     scaling_config {
-        desired_size = 1
-        max_size = 1
+        desired_size = 2
+        max_size = 2
         min_size = 1
     }
 
